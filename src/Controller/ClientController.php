4009a512241b930273981a7887eb673128d322e7
ch-egor <ch_egor.92@mail.ru>
2018-08-28 21:32:04 +0700

    activate client account through registration confirmation letter

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -6,10 +6,13 @@ use App\Entity\Client;
 use App\Form\ClientType;
 use App\Repository\ClientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * @Route("/client")
@@ -97,6 +100,45 @@ class ClientController extends Controller
         }
 
         return $this->redirectToRoute('client_index');
+    }
+
+    /**
+     * @Route("/{id}/activate/{code}", name="client_activate", methods="GET|POST")
+     */
+    public function activate(Request $request, UserPasswordEncoderInterface $passwordEncoder, Client $client, string $code): Response
+    {
+        if (empty($code) || $client->getEmailConfirmationCode() !== $code) {
+            throw $this->createNotFoundException('Wrong confirmation code');
+        }
+
+        $form = $this->createFormBuilder($client)
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+            ])
+            ->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword($client, $client->getPassword());
+            $client->setPassword($password);
+
+            $client->setEmailConfirmationCode(null);
+            $client->setIsActive(1);
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('client/activate.html.twig', [
+            'client' => $client,
+            'form' => $form->createView(),
+        ]);
     }
 
     private function replaceClientPhotoFileWithName(Client $client): void
