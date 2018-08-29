76c3d3a2054f2d7e4c22149c93168779ba52b4f5
ch-egor <ch_egor.92@mail.ru>
2018-08-29 15:15:23 +0700

    change Group Session's Description field type to textarea

--- src/Form/GroupSessionType.php
+++ src/Form/GroupSessionType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\GroupSession;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,9 @@ class GroupSessionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('coach')
-            ->add('description')
+            ->add('name', TextType::class)
+            ->add('coach', TextType::class)
+            ->add('description', TextareaType::class)
         ;
     }
 
